23d8abcd2ef3dfb183cd5ab0960111c0aec349aa
Mate V <47510515+03vmate@users.noreply.github.com>
2024-02-07 01:50:49 +0100

    Schematic and half-done layout

---
+++ pocket-ds18b20-fw/pocket-ds18b20-fw/main.c
@@ -0,0 +1,18 @@
+/*
+ * pocket-ds18b20-fw.c
+ *
+ * Created: 2/6/2024 5:47:37 PM
+ * Author : Mate
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
