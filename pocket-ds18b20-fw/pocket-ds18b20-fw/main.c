a804adc44c52eebf86eefc9f3c662fa30b702c87
Mate V <47510515+03vmate@users.noreply.github.com>
2024-09-01 04:55:31 +0200

    Finished fw

--- pocket-ds18b20-fw/pocket-ds18b20-fw/main.c
+++ pocket-ds18b20-fw/pocket-ds18b20-fw/main.c
@@ -47,6 +47,7 @@ void setMAXDigit(uint8_t digit, uint8_t val, bool dot) {
 	setMAXRegister(dig+1, val);
 }
 
+/*
 void setMAXInteger(uint16_t integer) {
 	integer %= 10000; // Trim extra digits that can't be displayed
 
@@ -58,6 +59,22 @@ void setMAXInteger(uint16_t integer) {
 	setMAXDigit(1, integer % 10, false);
 	integer /= 10;
 	setMAXDigit(0, integer, false);
+}
+*/
+
+void showVccVoltage(uint16_t adcval) {
+	// Bandgap voltage: (actualVcc * adcReading) / 1024
+	// Vcc: (bandgapVoltage * 1024) / adcReading
+	
+	uint16_t num = 1116860 / adcval; // Multiplied by 100 to not have to deal with floats
+	
+	setMAXDigit(0, num / 1000, true);
+	num = num % 1000;
+	setMAXDigit(1, num / 100, false);
+	num = num % 100;
+	setMAXDigit(2, num / 10, false);
+	setMAXDigit(3, num % 10, false);
+	
 }
 
 void setMAXfromDS(int16_t dstemp) {
@@ -87,6 +104,7 @@ void setMAXfromDS(int16_t dstemp) {
 }
 
 int main(void) {
+	_delay_ms(500);
 	DDRA |= (1 << CS);
 	DDRA |= (1 << SCK);
 	DDRA |= (1 << MOSI);
@@ -99,46 +117,34 @@ int main(void) {
 	ADCSRA = 0b10000100; // Enable ADC, prescaler 16
 	_delay_ms(1);
 
-	// Sum 1000 ADC readings
-	uint16_t adc_sum = 0;
-	for(uint16_t i = 0; i < 1000; i++) {
-		ADCSRA |= (1 << ADSC); // Start conversion
-		while(!(ADCSRA & (1 << ADIF))); // Wait for conversion to complete
-		ADCSRA |= (1 << ADIF); // Clear the ADC interrupt flag
-		volatile uint16_t adcl = ADCL; // Read lower byte
-		adc_sum += (((uint16_t)ADCH) << 8) | adcl; // Read upper byte and merge into uint16_t
-	}
-
-	adc_sum /= 1000; // Calculate the average
-	
-	
-	
-	
-	_delay_ms(10);
-
 	setMAXRegister(0x0C, 1);	// Shutdown -> Normal mode
 	setMAXRegister(0x0F, 0);	// Display Test -> Normal mode
 	setMAXRegister(0x09, 0x0F);	// Decode Mode -> Code B decode for digits 0-3
 	setMAXRegister(0x0A, 0);	// Intensity -> 0
 	setMAXRegister(0x0B, 3);	// Scan Limit -> 0,1,2,3
-
-	setMAXDigit(0, 8, true);
-	setMAXDigit(1, 8, true);
-	setMAXDigit(2, 8, true);
-	setMAXDigit(3, 8, true);
+	_delay_ms(200);
 
 	ds18b20wsp(&PORTB, &DDRB, &PINB, ( 1 << 2 ), 0, 0, 125, DS18B20_RES11);
 	
-	setMAXInteger(adc_sum);
-	_delay_ms(2000);
-
+	for(uint8_t j = 0; j < 30; j++) {
+		uint16_t adcsum = 0;
+		for(uint8_t i = 0; i < 100; i++) {
+			ADCSRA |= (1 << ADSC); // Start conversion
+			while(!(ADCSRA & (1 << ADIF))); // Wait for conversion to complete
+			ADCSRA |= (1 << ADIF); // Clear the ADC interrupt flag
+			volatile uint16_t adcl = ADCL; // Read lower byte
+			adcsum += (((uint16_t)ADCH) << 8) | adcl; // Read upper byte and merge into uint16_t
+			_delay_ms(1);
+		}
+		showVccVoltage(adcsum / 100);
+	}
+	
 	int16_t temp = 0;
 	while (1) {
 		ds18b20convert(&PORTB, &DDRB, &PINB, ( 1 << 2 ), 0);
 		_delay_ms(10);
 		ds18b20read(&PORTB, &DDRB, &PINB, ( 1 << 2 ), 0, &temp);
 		setMAXfromDS(temp);
-		
 	}
 	
 	
